ce32cdcc2d5c9f98b694527e7ceae900116f5969
jejwon <jeje5073@gmail.com>
2022-10-12 17:29:43 +0900

    10/12
    sql + java

---
+++ src/main/java/domain/ch14/Employee.java
@@ -0,0 +1,22 @@
+package domain.ch14;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	
+	//자바빈 만들기(ch14.servlet17)
+	//get, set 메소드 생성-> 프로퍼티 생김
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/sql/16like.sql
@@ -0,0 +1,20 @@
+-- LIKE : 패턴으로 레코드 조회
+-- wildcard(패턴에서 사용하는 특수 기호)
+-- : %(0개 이상의 문자), _(하나의 문자)
+
+SELECT * FROM Customers WHERE CustomerName LIKE 'A%';
+SELECT * FROM Customers WHERE CustomerName LIKE '%A';
+SELECT * FROM Customers WHERE CustomerName LIKE '%OR%';
+SELECT * FROM Customers WHERE CustomerName LIKE '_R%';
+SELECT * FROM Customers WHERE CustomerName LIKE 'A_%_%';
+SELECT * FROM Customers WHERE ContactName LIKE 'A%O';
+
+SELECT * FROM Employees WHERE FirstName LIKE 'A%';
+
+SELECT * FROM Suppliers WHERE ContactName LIKE '%E';
+
+SELECT * FROM Products WHERE ProductName LIKE 'C%S';
+
+SELECT * FROM Products WHERE ProductName LIKE 'C____';
+
+
